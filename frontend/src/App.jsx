4e48796d899dea9738863455f718a3edd9ea3c9b
calebvweich <calebvweich@gmail.com>
2025-10-18 00:04:20 +0100

    initiated components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/header/header'
+import Sidebar from './components/sidebar/sidebar'
+import RestTile from './components/restTile/restTile'
 
 function App() {
 
   return (
     <>
+    <Header />
+    <Sidebar />
+    <RestTile />
     </>
   )
 }
---
+++ frontend/src/components/restTile/restTile.jsx
@@ -0,0 +1,6 @@
+import "./restTile.css"
+export default function RestTile() {
+  return (
+    <div className="restTile">RestTile</div>
+  )
+}
