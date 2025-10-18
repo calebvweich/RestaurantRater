0c8e0c41e0031190cee9955031b612edcd9a1cb0
calebvweich <calebvweich@gmail.com>
2025-10-19 00:11:19 +0100

    Added header content

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react"
 import "./header.css"
 export default function Header() {
+  const [search, setSearch] = useState("")
   return (
-    <div className="header">Header</div>
+    <div className="header">
+      <h1>Rate-A-Rest</h1>
+      <input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="searchBar"
+      />
+      <button className="accButton">Account</button>
+    </div>
   )
 }
