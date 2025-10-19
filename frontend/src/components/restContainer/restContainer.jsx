b325d5c54ba1769256624d1d3a6eb426170c94b3
calebvweich <calebvweich@gmail.com>
2025-10-19 18:05:11 +0100

    Styled tiles to a 4-column list

--- frontend/src/components/restContainer/restContainer.jsx
+++ frontend/src/components/restContainer/restContainer.jsx
@@ -2,7 +2,8 @@ import RestTile from "../restTile/restTile"
 import "./restContainer.css"
 export default function RestContainer() {
   return (
-    <div className="restContainer">RestContainer
+    <div className="restContainer">
+      <h2>Closest Restaurants</h2>
       <div className="restGrid">
         <RestTile />
       </div>
