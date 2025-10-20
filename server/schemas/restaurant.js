b1e20b86807d72ca779a678330a4a91c709380bc
calebvweich <calebvweich@gmail.com>
2025-10-21 00:26:27 +0100

    updated restSchema, added connection string

--- server/schemas/restaurant.js
+++ server/schemas/restaurant.js
@@ -1,9 +1,28 @@
 import mongoose from "mongoose"
 
 const restSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  ratings: { up: { type: Number }, down: { type: Number } },
-  reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
-  location: { type: String },
-})
+  name: String,
+  address: String,
+  coords: {
+    lat: Number,
+    lng: Number
+  },
+  phone: String,
+  website: String,
+  image: String,
+  gallery: [String],
+  description: String,
+  categories: [String],
+  priceRange: String,   // "£", "££", "£££"
+  takeaway: Boolean,
+  delivery: Boolean,
+  reservations: Boolean,
+  openingHours: Object,
+  ratings: {
+    up: Number,
+    down: Number
+  },
+  reviews: [mongoose.Schema.Types.ObjectId]
+});
+
 export default mongoose.model("Rest", restSchema);
