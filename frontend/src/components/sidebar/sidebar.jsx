9d72dd233816f76fd927f4ec2b15fc354cf700a7
calebvweich <calebvweich@gmail.com>
2025-10-21 00:25:40 +0100

    updated sidebar styling

--- frontend/src/components/sidebar/sidebar.jsx
+++ frontend/src/components/sidebar/sidebar.jsx
@@ -2,7 +2,10 @@ import "./sidebar.css"
 export default function Sidebar() {
   return (
     <div className="sidebar">
-      <h3>Filters</h3>
+      <h3>Sort By</h3>
+      <div className="sortList"></div>
+      <h3>Categories</h3>
+      <div className="catList"></div>
     </div>
   )
 }
